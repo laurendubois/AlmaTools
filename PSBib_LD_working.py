7fa58383c7513cfcc97301185f86bd64b6a91bbd
Lauren D <144151898+laurendubois@users.noreply.github.com>
2024-04-03 09:47:58 -0500

    Update PSBib_LD_working.py
    Improved form of global variables

--- PSBib_LD_working.py
+++ PSBib_LD_working.py
@@ -3,14 +3,14 @@ import requests
 from bs4 import BeautifulSoup
 
 # Constants for file names
-api_key_file = 'PSB.txt'
-mms_ids_file = 'bibs.txt'
-output_file = 'holding_results.txt'
-error_file = 'holding_errors.txt'
-base_url = "https://api-na.hosted.exlibrisgroup.com/almaws/v1/"
+API_KEY_FILE = 'PSB.txt'
+MMS_IDS_FILE = 'bibs2.txt'
+OUTPUT_FILE = 'holding_results2.txt'
+ERROR_FILE = 'holding_errors2.txt'
+BASE_URL = "https://api-na.hosted.exlibrisgroup.com/almaws/v1/"
 
 # Read API key from file
-with open(api_key_file, 'r') as file:
+with open(API_KEY_FILE, 'r') as file:
     apikey = file.read().strip()
 
 # Basic request to check connection/API key
@@ -20,7 +20,7 @@ headers = {
     "Accept": "application/xml"
 }
 sys.stdout.write("Checking API key...")
-test_api = requests.post(base_url + "bibs/test", headers=headers)
+test_api = requests.post(BASE_URL + "bibs/test", headers=headers)
 if test_api.status_code == 400:
     print("\nInvalid API key - please confirm key has r/w permission for /bibs", )
     sys.exit()
@@ -31,12 +31,12 @@ else:
     sys.stdout.write("OK\n")
 
 # Read MMS IDs from file
-with open(mms_ids_file, 'r') as file:
+with open(MMS_IDS_FILE, 'r') as file:
     mms_ids = [line.strip() for line in file]
 
 # Function to retrieve holdings for a given MMS ID
 def get_holdings(mms_id):
-    url = f"{base_url}bibs/{mms_id}/holdings"
+    url = f"{BASE_URL}bibs/{mms_id}/holdings"
     response = requests.get(url, headers=headers)
     if response.status_code == 200:
         holdings_xml = response.text
@@ -70,13 +70,13 @@ for mms_id in mms_ids:
     if holdings_xml:
         holding_details = extract_holding_details(holdings_xml)
         if holding_details:
-            write_mms_and_holding_details(output_file, mms_id, holding_details)
-            print(f"Holding details for MMS ID {mms_id} appended to {output_file}")
+            write_mms_and_holding_details(OUTPUT_FILE, mms_id, holding_details)
+            print(f"Holding details for MMS ID {mms_id} appended to {OUTPUT_FILE}")
         else:
-            with open(error_file, 'a') as file:
+            with open(ERROR_FILE, 'a') as file:
                 file.write(f"No holding record for MMS ID: {mms_id}\n")
-            print(f"No holding record for MMS ID {mms_id}. Written to {error_file}")
+            print(f"No holding record for MMS ID {mms_id}. Written to {ERROR_FILE}")
     else:
-        with open(error_file, 'a') as file:
+        with open(ERROR_FILE, 'a') as file:
             file.write(f"Failed to retrieve holdings for MMS ID: {mms_id}\n")
-        print(f"Failed MMS ID: {mms_id}. Written to {error_file}")
+        print(f"Failed MMS ID: {mms_id}. Written to {ERROR_FILE}")
